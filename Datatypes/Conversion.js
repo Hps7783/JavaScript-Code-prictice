c08c1e4d52b8a565d3f8a2ed4a91041e73faed6a
Hemant Kumar Sharma <137736113+Hps7783@users.noreply.github.com>
2024-04-11 17:12:34 +0000

    add data

--- Datatypes/Conversion.js
+++ Datatypes/Conversion.js
@@ -7,8 +7,27 @@ console.log(typeof (score));
 
 // number to string
 
+let number = "100";     // this number will string type becouse 100 is inside of  " " doubel qoutes
+console.log(number);
+console.log(typeof (number));
+
+console.log("line end 2");
+
+// example if 100 is not in double qoutes then
+ let num = 20;
+ let valueOfString = String(num);
+ console.log(typeof(num));
+ console.log(valueOfString);
+
+console.log("line end 3");
 
 // string to number
+let num1 = 10;
+let valueInNumber = Number(num1);
+console.log(typeof (num1));
+console.log(valueInNumber);
+
+console.log("line end 4");
 
 // number to boolean
 
