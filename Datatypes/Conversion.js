a0ce7a6e7c4887c37097da6265b20a7266ff296b
Hemant Kumar Sharma <137736113+Hps7783@users.noreply.github.com>
2024-04-11 16:53:21 +0000

    data type conversion

--- Datatypes/Conversion.js
+++ Datatypes/Conversion.js
@@ -1,4 +1,12 @@
+console.log("Node Js With Hemant");
+
+// print number and find the data type
+let score = 100;
+console.log(score);
+console.log(typeof (score));
+
 // number to string
+
 
 // string to number
 
